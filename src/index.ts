0cd4b45468eab722947f372068f01055c5d5a3e3
Dan Crescimanno <dcrmls@gmail.com>
2021-10-12 14:02:10 -0700

    Added MaxAbsScaler

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,18 @@
 * ==========================================================================
 */
 
-import MinMaxScaler from "./preprocessing/scalers/min.max.scaler"
-import StandardScaler from "./preprocessing/scalers/standard.scaler"
-
-import getDummies from "./preprocessing/encoders/dummy.encoder"
-import OneHotEncoder from "./preprocessing/encoders/one.hot.encoder"
-import LabelEncoder from "./preprocessing/encoders/label.encoder"
+import MinMaxScaler from './preprocessing/scalers/min.max.scaler'
+import StandardScaler from './preprocessing/scalers/standard.scaler'
+import MaxAbsScaler from './preprocessing/scalers/max.abs.scaler'
+import getDummies from './preprocessing/encoders/dummy.encoder'
+import OneHotEncoder from './preprocessing/encoders/one.hot.encoder'
+import LabelEncoder from './preprocessing/encoders/label.encoder'
 
 export {
-    MinMaxScaler,
-    StandardScaler,
-    getDummies,
-    OneHotEncoder,
-    LabelEncoder
+  MinMaxScaler,
+  StandardScaler,
+  MaxAbsScaler,
+  getDummies,
+  OneHotEncoder,
+  LabelEncoder,
 }
---
+++ src/preprocessing/scalers/max.abs.scaler.ts
@@ -0,0 +1,107 @@
+/**
+*  @license
+* Copyright 2021, JsData. All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ==========================================================================
+*/
+
+import { Tensor1D, tensor1d, zerosLike } from '@tensorflow/tfjs-node'
+import {
+  ScikitVecOrMatrix,
+  convertToNumericTensor1D_2D,
+  convertTensorToInputType,
+  minIgnoreNaN,
+  maxIgnoreNaN,
+  turnZerosToOnes,
+} from '../../utils'
+/**
+ * Transform features by scaling each feature to a given range.
+ * This estimator scales and translates each feature individually such
+ * that it is in the given range on the training set, e.g. between the maximum and minimum value.
+ */
+
+export default class MaxAbsScaler {
+  $scale: Tensor1D
+
+  constructor() {
+    this.$scale = tensor1d([])
+  }
+
+  /**
+   * Fits a MinMaxScaler to the data
+   * @param data Array, Tensor, DataFrame or Series object
+   * @returns MinMaxScaler
+   * @example
+   * const scaler = new MinMaxScaler()
+   * scaler.fit([1, 2, 3, 4, 5])
+   * // MinMaxScaler {
+   * //   $max: [5],
+   * //   $min: [1]
+   * // }
+   *
+   */
+  fit(data: ScikitVecOrMatrix) {
+    const tensorArray = convertToNumericTensor1D_2D(data)
+    const scale = maxIgnoreNaN(tensorArray.abs(), 0) as Tensor1D
+
+    // Deal with 0 scale values
+    this.$scale = turnZerosToOnes(scale) as Tensor1D
+
+    return this
+  }
+
+  /**
+   * Transform the data using the fitted scaler
+   * @param data Array, Tensor, DataFrame or Series object
+   * @returns Array, Tensor, DataFrame or Series object
+   * @example
+   * const scaler = new MinMaxScaler()
+   * scaler.fit([1, 2, 3, 4, 5])
+   * scaler.transform([1, 2, 3, 4, 5])
+   * // [0, 0.25, 0.5, 0.75, 1]
+   * */
+  transform(data: ScikitVecOrMatrix) {
+    const tensorArray = convertToNumericTensor1D_2D(data)
+    const outputData = tensorArray.div(this.$scale)
+    return convertTensorToInputType(outputData, data)
+  }
+
+  /**
+   * Fit the data and transform it
+   * @param data Array, Tensor, DataFrame or Series object
+   * @returns Array, Tensor, DataFrame or Series object
+   * @example
+   * const scaler = new MinMaxScaler()
+   * scaler.fitTransform([1, 2, 3, 4, 5])
+   * // [0, 0.25, 0.5, 0.75, 1]
+   * */
+  fitTransform(data: ScikitVecOrMatrix) {
+    // Should we just have a mixin that does this?
+    this.fit(data)
+    return this.transform(data)
+  }
+
+  /**
+   * Inverse transform the data using the fitted scaler
+   * @param data Array, Tensor, DataFrame or Series object
+   * @returns Array, Tensor, DataFrame or Series object
+   * @example
+   * const scaler = new MinMaxScaler()
+   * scaler.fit([1, 2, 3, 4, 5])
+   * scaler.inverseTransform([0, 0.25, 0.5, 0.75, 1])
+   * // [1, 2, 3, 4, 5]
+   * */
+  inverseTransform(data: ScikitVecOrMatrix) {
+    const tensorArray = convertToNumericTensor1D_2D(data)
+    const outputData = tensorArray.mul(this.$scale)
+    return convertTensorToInputType(outputData, data)
+  }
+}
