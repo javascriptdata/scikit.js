cc3993161d536734a4f26ad6ca02722c59dd45eb
Dan Crescimanno <dcrmls@gmail.com>
2022-01-19 09:49:08 -0800

    chore: slight tweaks to make it more general

--- src/datasets/makeRegression.ts
+++ src/datasets/makeRegression.ts
@@ -1,8 +1,5 @@
-import { Scikit1D, Scikit2D } from '../types'
-import { min } from 'mathjs'
 import { tf } from '../shared/globals'
-import { makeLowRankMatrix } from './makeLowRankMatrix'
-
+type Tensor = tf.Tensor
 interface MakeRegressionInput {
   nSamples?: number
   nFeatures?: number
@@ -16,9 +13,7 @@ interface MakeRegressionInput {
   coef?: boolean
 }
 
-type MakeRegressionOutput =
-  | [Scikit2D, Scikit1D]
-  | [Scikit2D, Scikit1D, Scikit1D]
+type MakeRegressionOutput = [Tensor, Tensor] | [Tensor, Tensor, Tensor]
 
 export const makeRegression = ({
   nSamples = 100,
@@ -32,7 +27,7 @@ export const makeRegression = ({
   shuffle = false,
   coef = false
 }: MakeRegressionInput = {}): MakeRegressionOutput => {
-  const numberInformative = min(nFeatures, nInformative)
+  const numberInformative = Math.min(nFeatures, nInformative)
 
   let X: tf.Tensor2D
   if (effectiveRank === null) {
@@ -45,39 +40,62 @@ export const makeRegression = ({
   // Generate a ground truth model with only n_informative features being non
   // zeros (the other features are not correlated to y and should be ignored
   // by a sparsifying regularizers such as L1 or elastic net)
-  let [groundTruthA, groundTruthB] = tf.split(
-    tf.zeros([nFeatures, nTargets]),
-    [numberInformative, nFeatures - numberInformative],
-    0
-  )
-  let groundTruthRandom = tf.randomNormal([numberInformative, nTargets])
-  groundTruthRandom = groundTruthRandom.mul(
-    tf.fill(groundTruthRandom.shape, 100)
-  )
-  groundTruthA = groundTruthA.add(groundTruthRandom)
 
-  let groundTruth = tf.concat([groundTruthA, groundTruthB])
-
-  let Y: tf.Tensor1D = tf.einsum('i,i->', X, groundTruth).as1D()
-
-  Y = Y.add(tf.fill(Y.shape, bias)).as1D()
+  const model = tf.randomNormal([numberInformative, nTargets]).mul(100)
+  const zeros = tf.zeros([nFeatures - numberInformative, nTargets])
+  const groundTruth = tf.concat([model, zeros])
+  let Y = X.dot(groundTruth).add(bias)
 
   // Add noise
   if (noise > 0) {
-    Y = Y.add(tf.randomNormal(Y.shape, undefined, noise)).as1D()
+    Y = Y.add(tf.randomNormal(Y.shape, undefined, noise))
   }
 
   // Randomly permute samples and features
   if (shuffle) {
     const randomTen = tf.util.createShuffledIndices(nSamples)
 
-    X.gather(Array.from(randomTen))
+    X = X.gather(randomTen)
   }
 
-  Y = tf.squeeze(Y).as1D()
+  Y = tf.squeeze(Y)
 
   if (coef) {
-    return [X, Y, tf.squeeze(groundTruth).as1D()]
+    return [X, Y, tf.squeeze(groundTruth)]
   }
   return [X, Y]
+}
+
+interface MakeLowRankMatrixInput {
+  nSamples?: number
+  nFeatures?: number
+  effectiveRank?: number
+  tailStrength?: number
+}
+
+export const makeLowRankMatrix = ({
+  nSamples = 100,
+  nFeatures = 100,
+  effectiveRank = 10,
+  tailStrength = 0.5
+}: MakeLowRankMatrixInput = {}): tf.Tensor2D => {
+  let n = Math.min(nSamples, nFeatures)
+
+  // Random (ortho normal) vectors
+  let [u] = tf.linalg.qr(tf.randomNormal([nSamples, n]))
+  let [v] = tf.linalg.qr(tf.randomNormal([nFeatures, n]))
+
+  // Index of the singular values
+  let singularIndex = tf.linspace(0, n - 1, n)
+
+  // Build the singular profile by assembling signal and noise components
+  const singularIndexByRank = singularIndex.div(effectiveRank)
+  const lowRank = tf
+    .exp(singularIndexByRank.square().neg())
+    .mul(1 - tailStrength)
+  let tail = tf.exp(singularIndexByRank.mul(-0.1)).mul(tailStrength)
+
+  let s = tf.diag(lowRank.add(tail))
+
+  return u.dot(s).dot(v.transpose()) as tf.Tensor2D
 }
