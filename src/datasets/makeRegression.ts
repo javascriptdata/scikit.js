441ca305c24c38325b58a7a784db9446ac007d75
Dan Crescimanno <dcrmls@gmail.com>
2022-01-19 11:31:17 -0800

    chore: added in memory management

--- src/datasets/makeRegression.ts
+++ src/datasets/makeRegression.ts
@@ -27,43 +27,50 @@ export const makeRegression = ({
   shuffle = false,
   coef = false
 }: MakeRegressionInput = {}): MakeRegressionOutput => {
-  const numberInformative = Math.min(nFeatures, nInformative)
+  return tf.tidy(() => {
+    const numberInformative = Math.min(nFeatures, nInformative)
 
-  let X: tf.Tensor2D
-  if (effectiveRank === null) {
-    // Randomly generate a well conditioned input set
-    X = tf.randomNormal([nSamples, nFeatures])
-  } else {
-    X = makeLowRankMatrix({ nSamples, nFeatures, effectiveRank, tailStrength })
-  }
+    let X: tf.Tensor2D
+    if (effectiveRank === null) {
+      // Randomly generate a well conditioned input set
+      X = tf.randomNormal([nSamples, nFeatures])
+    } else {
+      X = makeLowRankMatrix({
+        nSamples,
+        nFeatures,
+        effectiveRank,
+        tailStrength
+      })
+    }
 
-  // Generate a ground truth model with only n_informative features being non
-  // zeros (the other features are not correlated to y and should be ignored
-  // by a sparsifying regularizers such as L1 or elastic net)
+    // Generate a ground truth model with only n_informative features being non
+    // zeros (the other features are not correlated to y and should be ignored
+    // by a sparsifying regularizers such as L1 or elastic net)
 
-  const model = tf.randomNormal([numberInformative, nTargets]).mul(100)
-  const zeros = tf.zeros([nFeatures - numberInformative, nTargets])
-  const groundTruth = tf.concat([model, zeros])
-  let Y = X.dot(groundTruth).add(bias)
+    const model = tf.randomNormal([numberInformative, nTargets]).mul(100)
+    const zeros = tf.zeros([nFeatures - numberInformative, nTargets])
+    const groundTruth = tf.concat([model, zeros])
+    let Y = X.dot(groundTruth).add(bias)
 
-  // Add noise
-  if (noise > 0) {
-    Y = Y.add(tf.randomNormal(Y.shape, undefined, noise))
-  }
+    // Add noise
+    if (noise > 0) {
+      Y = Y.add(tf.randomNormal(Y.shape, undefined, noise))
+    }
 
-  // Randomly permute samples and features
-  if (shuffle) {
-    const randomTen = tf.util.createShuffledIndices(nSamples)
+    // Randomly permute samples and features
+    if (shuffle) {
+      const randomTen = tf.util.createShuffledIndices(nSamples)
 
-    X = X.gather(randomTen)
-  }
+      X = X.gather(randomTen)
+    }
 
-  Y = tf.squeeze(Y)
+    Y = tf.squeeze(Y)
 
-  if (coef) {
-    return [X, Y, tf.squeeze(groundTruth)]
-  }
-  return [X, Y]
+    if (coef) {
+      return [X, Y, tf.squeeze(groundTruth)]
+    }
+    return [X, Y]
+  })
 }
 
 interface MakeLowRankMatrixInput {
@@ -79,23 +86,25 @@ export const makeLowRankMatrix = ({
   effectiveRank = 10,
   tailStrength = 0.5
 }: MakeLowRankMatrixInput = {}): tf.Tensor2D => {
-  let n = Math.min(nSamples, nFeatures)
+  return tf.tidy(() => {
+    let n = Math.min(nSamples, nFeatures)
 
-  // Random (ortho normal) vectors
-  let [u] = tf.linalg.qr(tf.randomNormal([nSamples, n]))
-  let [v] = tf.linalg.qr(tf.randomNormal([nFeatures, n]))
+    // Random (ortho normal) vectors
+    let [u] = tf.linalg.qr(tf.randomNormal([nSamples, n]))
+    let [v] = tf.linalg.qr(tf.randomNormal([nFeatures, n]))
 
-  // Index of the singular values
-  let singularIndex = tf.linspace(0, n - 1, n)
+    // Index of the singular values
+    let singularIndex = tf.linspace(0, n - 1, n)
 
-  // Build the singular profile by assembling signal and noise components
-  const singularIndexByRank = singularIndex.div(effectiveRank)
-  const lowRank = tf
-    .exp(singularIndexByRank.square().neg())
-    .mul(1 - tailStrength)
-  let tail = tf.exp(singularIndexByRank.mul(-0.1)).mul(tailStrength)
+    // Build the singular profile by assembling signal and noise components
+    const singularIndexByRank = singularIndex.div(effectiveRank)
+    const lowRank = tf
+      .exp(singularIndexByRank.square().neg())
+      .mul(1 - tailStrength)
+    let tail = tf.exp(singularIndexByRank.mul(-0.1)).mul(tailStrength)
 
-  let s = tf.diag(lowRank.add(tail))
+    let s = tf.diag(lowRank.add(tail))
 
-  return u.dot(s).dot(v.transpose()) as tf.Tensor2D
+    return u.dot(s).dot(v.transpose()) as tf.Tensor2D
+  })
 }
