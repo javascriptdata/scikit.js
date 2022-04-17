dd952567e6b41c5e314e99f62debae4e81b9d0f5
Dan Crescimanno <dcrmls@gmail.com>
2022-04-17 10:11:11 -0700

    feat: commenting out svc, svr code until it can be built in browser

--- src/svm/SVR.ts
+++ src/svm/SVR.ts
@@ -1,102 +1,102 @@
-import { tf } from '../shared/globals'
-import { Scikit1D, Scikit2D } from '../index'
-import { SVM, SVMParam, KERNEL_TYPE, ISVMParam, SVM_TYPE } from 'libsvm-wasm'
-import { convertToNumericTensor1D, convertToNumericTensor2D } from '../utils'
-import { assert } from '../typesUtils'
+// import { tf } from '../shared/globals'
+// import { Scikit1D, Scikit2D } from '../index'
+// import { SVM, SVMParam, KERNEL_TYPE, ISVMParam, SVM_TYPE } from 'libsvm-wasm'
+// import { convertToNumericTensor1D, convertToNumericTensor2D } from '../utils'
+// import { assert } from '../typesUtils'
 
-export interface SVRParams {
-  kernel?: 'LINEAR' | 'POLY' | 'RBF' | 'SIGMOID' | 'PRECOMPUTED'
-  degree?: number
-  gamma?: number | 'auto' | 'scale'
-  coef0?: number
-  tol?: number
-  C?: number
-  epsilon?: number
-  shrinking?: boolean
-  cacheSize?: number
-  maxIter?: number
-}
+// export interface SVRParams {
+//   kernel?: 'LINEAR' | 'POLY' | 'RBF' | 'SIGMOID' | 'PRECOMPUTED'
+//   degree?: number
+//   gamma?: number | 'auto' | 'scale'
+//   coef0?: number
+//   tol?: number
+//   C?: number
+//   epsilon?: number
+//   shrinking?: boolean
+//   cacheSize?: number
+//   maxIter?: number
+// }
 
-export class SVR {
-  private svm?: SVM
-  private svmParam: SVMParam
-  private gammaMode = 'scale'
+// export class SVR {
+//   private svm?: SVM
+//   private svmParam: SVMParam
+//   private gammaMode = 'scale'
 
-  constructor({
-    kernel = 'RBF',
-    degree = 3,
-    gamma = 'scale',
-    coef0 = 0,
-    tol = 1e-3,
-    C = 1,
-    epsilon = 0.1,
-    shrinking = true,
-    cacheSize = 200,
-    maxIter = -1
-  }: SVRParams = {}) {
-    const internalSVMParam: ISVMParam = {
-      kernel_type: KERNEL_TYPE[kernel],
-      svm_type: SVM_TYPE.EPSILON_SVR,
-      degree,
-      coef0,
-      C,
-      p: epsilon,
-      shrinking: shrinking ? 1 : 0,
-      cache_size: cacheSize
-    }
-    if (gamma === 'auto') {
-      this.gammaMode = gamma
-    } else if (gamma === 'scale') {
-      this.gammaMode = gamma
-    } else {
-      internalSVMParam.gamma = gamma
-    }
+//   constructor({
+//     kernel = 'RBF',
+//     degree = 3,
+//     gamma = 'scale',
+//     coef0 = 0,
+//     tol = 1e-3,
+//     C = 1,
+//     epsilon = 0.1,
+//     shrinking = true,
+//     cacheSize = 200,
+//     maxIter = -1
+//   }: SVRParams = {}) {
+//     const internalSVMParam: ISVMParam = {
+//       kernel_type: KERNEL_TYPE[kernel],
+//       svm_type: SVM_TYPE.EPSILON_SVR,
+//       degree,
+//       coef0,
+//       C,
+//       p: epsilon,
+//       shrinking: shrinking ? 1 : 0,
+//       cache_size: cacheSize
+//     }
+//     if (gamma === 'auto') {
+//       this.gammaMode = gamma
+//     } else if (gamma === 'scale') {
+//       this.gammaMode = gamma
+//     } else {
+//       internalSVMParam.gamma = gamma
+//     }
 
-    this.svmParam = new SVMParam(internalSVMParam, tol)
-  }
+//     this.svmParam = new SVMParam(internalSVMParam, tol)
+//   }
 
-  async fit(X: Scikit2D, y: Scikit1D): Promise<SVR> {
-    let XTwoD = convertToNumericTensor2D(X)
-    let yOneD = convertToNumericTensor1D(y)
-    let nSample = XTwoD.shape[0]
-    let nFeature = XTwoD.shape[1]
-    assert(
-      yOneD.shape[0] === nSample,
-      'X and y must have the same number of samples'
-    )
-    assert(yOneD.shape[0] >= 1, 'Must have more than 1 sample in X, and y')
+//   async fit(X: Scikit2D, y: Scikit1D): Promise<SVR> {
+//     let XTwoD = convertToNumericTensor2D(X)
+//     let yOneD = convertToNumericTensor1D(y)
+//     let nSample = XTwoD.shape[0]
+//     let nFeature = XTwoD.shape[1]
+//     assert(
+//       yOneD.shape[0] === nSample,
+//       'X and y must have the same number of samples'
+//     )
+//     assert(yOneD.shape[0] >= 1, 'Must have more than 1 sample in X, and y')
 
-    // Sum((XTwoD - Mean) ** 2) / nSample
-    const VarianceOfX = tf
-      .squaredDifference(XTwoD, XTwoD.mean())
-      .sum()
-      .div(nSample)
-      .dataSync()[0]
+//     // Sum((XTwoD - Mean) ** 2) / nSample
+//     const VarianceOfX = tf
+//       .squaredDifference(XTwoD, XTwoD.mean())
+//       .sum()
+//       .div(nSample)
+//       .dataSync()[0]
 
-    if (this.gammaMode === 'scale') {
-      this.svmParam.param.gamma = 1 / (nFeature * VarianceOfX)
-    } else if (this.gammaMode === 'auto') {
-      this.svmParam.param.gamma = 1 / nFeature
-    }
+//     if (this.gammaMode === 'scale') {
+//       this.svmParam.param.gamma = 1 / (nFeature * VarianceOfX)
+//     } else if (this.gammaMode === 'auto') {
+//       this.svmParam.param.gamma = 1 / nFeature
+//     }
 
-    const [processX, processY] = await Promise.all([
-      XTwoD.array(),
-      yOneD.array()
-    ])
+//     const [processX, processY] = await Promise.all([
+//       XTwoD.array(),
+//       yOneD.array()
+//     ])
 
-    this.svm = new SVM(this.svmParam)
-    await this.svm.init()
-    await this.svm.feedSamples(processX, processY)
-    await this.svm.train()
-    return this
-  }
+//     this.svm = new SVM(this.svmParam)
+//     await this.svm.init()
+//     await this.svm.feedSamples(processX, processY)
+//     await this.svm.train()
+//     return this
+//   }
 
-  predict(X: Scikit2D): tf.Tensor1D {
-    const XTensor = convertToNumericTensor2D(X)
-    const processX = XTensor.arraySync()
-    assert(Boolean(this.svm), 'SVM was not trained')
+//   predict(X: Scikit2D): tf.Tensor1D {
+//     const XTensor = convertToNumericTensor2D(X)
+//     const processX = XTensor.arraySync()
+//     assert(Boolean(this.svm), 'SVM was not trained')
 
-    const results = processX.map((el) => (this.svm as any).predict(el))
-    return tf.tensor1d(results)
-  }
-}
+//     const results = processX.map((el) => (this.svm as any).predict(el))
+//     return tf.tensor1d(results)
+//   }
+// }
