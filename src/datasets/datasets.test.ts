3251738e09b9e1af9a354b225033a57b1081f573
Dan Crescimanno <dcrmls@gmail.com>
2022-04-18 18:53:07 -0700

    feat: remove data loading logic in favor of using dfd.readCSV(url)

--- src/datasets/datasets.test.ts
+++
@@ -1,9 +0,0 @@
-import { loadBoston } from './datasets'
-
-describe('test', function () {
-  it('loadBoston test', async function () {
-    let Array2D = await loadBoston()
-    expect(Array2D.length).toEqual(507)
-    expect(Array2D[0].length).toEqual(14)
-  })
-})
