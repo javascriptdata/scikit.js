d3814caff4e204a0e919977ffec3686f69e28d24
Dan Crescimanno <dcrmls@gmail.com>
2022-05-09 23:11:16 -0700

    feat: removed unneeded build steps

--- build/nodeReplacer.js
+++
@@ -1,8 +0,0 @@
-exports.default = function exampleReplacer({
-  orig
-  // file
-}) {
-  if (orig.includes('/shared/globals'))
-    return orig.replace('/shared/globals', '/shared-node/globals')
-  return orig
-}
